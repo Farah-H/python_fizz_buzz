91bf12cae3cb6bbc345d829018e2f1be55151ad7
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-05 16:46:48 +0000

    deleted extra file, small edits

--- fizz_buzz_class.py
+++ fizz_buzz_class.py
@@ -24,16 +24,3 @@ class FizzBuzz():
                 numbers.append(number)
         return number
         
-
-
-
-fizz = int(input('What number would you like to replace multiples of with "Fizz"?'))
-buzz = int(input('What number would you like to replace multiples of with "Buzz"?'))
-start = int(input('Where would you like to start?'))
-end = int(input('Where would you like to end?'))
-
-if start != 0:
-    test = FizzBuzz(fizz, buzz, start, end)
-    print(test.check_numbers())
-else:
-    print('Please enter a non-zero integer.')
--- run_fizz_buzz.py
+++ run_fizz_buzz.py
@@ -5,8 +5,8 @@ buzz = int(input('What number would you like to replace multiples of with "Buzz"
 start = int(input('Where would you like to start?'))
 end = int(input('Where would you like to end?'))
 
-if start != 0:
+if 0 not in (start, end, fizz, buzz):
     test = FizzBuzz(fizz, buzz, start, end)
     print(test.check_numbers())
 else:
-    print('Please enter a non-zero integer.')
+    print('0 cannot be in the range of start, end nor can it be Fizz or Buzz, please try another input.')
